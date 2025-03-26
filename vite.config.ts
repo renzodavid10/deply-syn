6a9259c5c88e2dfe769512d36cd73124c281ee22
RenzoDavid110 <renzo.david110@gmail.com>
2025-03-26 09:35:15 -0500

    add

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
-  base: "https://renzodavid10.github.io/deply-syn/",
+  base: "/deply-syn/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
