992281270268ee4b9ebde874b73c06fc200c68b0
RenzoDavid110 <renzo.david110@gmail.com>
2025-03-26 08:21:34 -0500

    update

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ import { BrowserRouter } from 'react-router'
 createRoot(document.getElementById('root')!).render(
   <>
     <Provider store={store}>
-      <BrowserRouter basename='/deply-syn/'>
+      <BrowserRouter basename="/deply-syn">
         <AppRouter />
       </BrowserRouter>
       <Toaster />
