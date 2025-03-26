01177db4791bdd9a9b1e750e4cd636e7524503ff
RenzoDavid110 <renzo.david110@gmail.com>
2025-03-26 08:09:53 -0500

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()
-  ],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  base: "/deply-syn/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+
 })
